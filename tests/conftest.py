89aacac819232d57ea29a4392fc3fcecd9dc663d
Pradeep Pillai <mail_pillai@yahoo.com>
2020-09-23 17:20:22 +0530

    adding git ignore

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 import pytest
+import logging
+import time
 
+
+# client fixtures
 
 @pytest.fixture(scope="session")
 def sbdb_close_approach_data_client():
@@ -9,9 +13,6 @@ def sbdb_close_approach_data_client():
 
 @pytest.fixture(autouse=True)
 def logger(request):
-    import logging
-    import time
-
     log = logging.getLogger(__name__)
     log.info(f"\nStarting Test : {request.node.name} : {time.asctime(time.localtime())}\n")
     return log
