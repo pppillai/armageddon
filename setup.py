8a599c9162300aecc1a038107085d4c0166188e2
Pradeep Pillai <mail_pillai@yahoo.com>
2020-09-26 14:07:53 +0530

    adding web behaviour to the tool

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ setup(
     version="01",
     author="pradeep",
     install_requires=[
-        "pytest==5.3.4",
-        "pytest-html==2.1.1",
+        "pytest",
+        "pytest-html",
         "loguru==0.5.0",
         "jsons==1.2",
         "requests==2.22.0",
         "requests-toolbelt==0.9.1",
         "flask",
-        "gunicorn"
+        "gunicorn", 'markupsafe'
     ]
 )
