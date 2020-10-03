e7bea33eaa1739af5c1d892e6dddc02f613dc542
Pradeep Pillai <mail_pillai@yahoo.com>
2020-10-03 23:19:07 +0530

    remove unnecssary comments

--- clients/config.py
+++ clients/config.py
@@ -1,5 +1,5 @@
 class Config:
     TIMEOUT=10
-    KUBECLUSTERIP = None    # put None to fetch from environment
-    NODEPORTPORT = None              # put None to fetch from environment
+    KUBECLUSTERIP = None
+    NODEPORTPORT = None
     NASA_SBDB_URL = "https://ssd-api.jpl.nasa.gov/cad.api"
